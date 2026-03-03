23fcdf333c9199bbb097fe98921dc716c86c7904
jessebithealth <jesse@bithealth.co.id>
2026-03-03 19:24:42 +0700

    added logic for article web view

--- NewsApp/Articles/Web View/ArticlesWebViewViewController.swift
+++ NewsApp/Articles/Web View/ArticlesWebViewViewController.swift
@@ -6,10 +6,61 @@
 //
 
 import UIKit
+import WebKit
 
 class ArticlesWebViewViewController: UIViewController {
-
+    private var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Title"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+                image: UIImage(systemName: "chevron.left"),
+                style: .plain,
+                target: self,
+                action: #selector(handleBack)
+        )
+        setupWebView()
+        loadPage()
+    }
+    
+    @objc private func handleBack() {
+        if webView.canGoBack {
+            webView.goBack()
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    private func setupWebView() {
+        webView = WKWebView(frame: .zero)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.navigationDelegate = self
+        
+        view.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    private func loadPage() {
+        guard let url = URL(string: "https://www.apple.com") else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+}
+
+extension ArticlesWebViewViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+        hideLoading()
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showLoading()
     }
 }
