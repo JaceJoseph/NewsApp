a842ee4bac445e5bc4ccb76f2ba1f2cd426491d6
jessebithealth <jesse@bithealth.co.id>
2026-03-03 22:23:30 +0700

    fix empty state, fix category sports display name, fix category table cell constraints

--- NewsApp/Categories/View Model/CategoryViewModel.swift
+++ NewsApp/Categories/View Model/CategoryViewModel.swift
@@ -11,7 +11,7 @@ class CategoryViewModel {
         ArticleCategory(id: "general", title: "GENERAL", description: "For your general catchup for news"),
         ArticleCategory(id: "business", title: "BUSINESS", description: "Catchup to the latest economic and business news"),
         ArticleCategory(id: "technology", title: "TECHNOLOGY", description: "Keep up with the latest technology"),
-        ArticleCategory(id: "sports", title: "GENERAL", description: "Find out about your latest favourite sports news"),
+        ArticleCategory(id: "sports", title: "SPORTS", description: "Find out about your latest favourite sports news"),
         ArticleCategory(id: "entertainment", title: "ENTERTAINMENT", description: "For your latest entertainment purposes"),
         ArticleCategory(id: "science", title: "SCIENCE", description: "Keep up with the latest scientific findings")
     ]
---
+++ NewsApp/Helpers/Empty State/EmptyStateView.swift
@@ -0,0 +1,30 @@
+//
+//  EmptyStateView.swift
+//  NewsApp
+//
+//  Created by Jesse on 03/03/26.
+//
+
+import UIKit
+
+class EmptyStateView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let nib = UINib(nibName: "EmptyStateView", bundle: nil)
+        guard let contentView = nib.instantiate(withOwner: self).first as? UIView else { return }
+        
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(contentView)
+    }
+
+}
